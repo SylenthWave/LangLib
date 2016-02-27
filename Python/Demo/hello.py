3e19f609313fec51be41da7b7100ac8c371d5b36
SylenthWave <sylenthwave@gmail.com>
2016-02-27 10:53:20 +0800

    Add demo dir

---
+++ Python/Demo/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+print('Please enter your name:')
+username = input()
+print('Ok, Now enter \'q\' input your name')
+key = input()
+
+if key == 'q':
+    print('hello,',username)
